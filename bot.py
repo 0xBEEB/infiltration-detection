d3ba143315c861bf0e5048a776396b1487a24457
Thomas Schreiber <ubiquill@gmail.com>
2015-04-11 07:52:34 -0700

    Pulls from wiki & Refactoring
    The bot now pulls the list of subs to check from a wiki page.
    
    I also refacrtored all of the code that is used to parse out comments
    and posts.

--- bot.py
+++ bot.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 import time
-import praw
 import sys
+import HTMLParser
+import praw
 from models import User
+from reporter import UserReport
 from config import settings
 
 
 class Bot:
 
-    def __init__(self, username, password, subs):
+    def __init__(self, username, password):
         self.reddit = praw.Reddit('PRAW infiltration detection by /u/flaxrabbit v 1.0. ' +
                         'Url: http://github.com/ubiquill/infiltration-detection')
         self.reddit.login(username, password)
+        self.update_from_wiki()
 
-        self.suspicious_subs = subs
+    def update_from_wiki(self):
+        try:
+            subreddit = self.reddit.get_subreddit(settings['subreddit'])
+            page = subreddit.get_wiki_page(settings['wiki_page_name'])
+        except Exception:
+            return False
+
+        html_parser = HTMLParser.HTMLParser()
+        page_content = html_parser.unescape(page.content_md)
+
+        self.suspicious_subs = page_content.split()
 
     def check_and_respond(self):
         new_messages = self.reddit.get_unread(limit=None)
@@ -21,18 +34,17 @@ class Bot:
         for msg in new_messages:
             msg.mark_as_read()
             user_to_infiltrate = self.get_user_name_from_message(msg.body)
-            user = User(user_to_infiltrate, self.reddit, self.suspicious_subs, 50)
-            user.process_comments()
-            user.process_submitted()
+            user = User(user_to_infiltrate, self.reddit)
 
-            info = user.get_monitoring_info()
+            report = UserReport(user, self.suspicious_subs)
+            info = report.write()
 
             if len(info) > 0:
                 info += "\n\n___\n\n^I'm ^a ^bot. ^Only ^the ^past ^1,000 ^comments ^are ^fetched."
                 info.replace("http://www.", "http://np.")
                 info = info[:10000]
-            msg.reply(info)
 
+            msg.reply(info)
 
     def get_user_name_from_message(self, body):
         possible_users = [user for user in body.split() if user.startswith('/u/')]
@@ -45,20 +57,25 @@ class Bot:
         if len(possible_users) > 0:
             return possible_users[0]
 
+
 def main():
-    bot = Bot(settings['username'],
-              settings['password'],
-              settings['suspicious_subs'])
+    count = 0
+    bot = Bot(settings['username'], settings['password'])
 
     while True:
         try:
+            time.sleep(settings['delay'])
             bot.check_and_respond()
+            if count == 5:
+                bot.update_from_wiki()
+                count = 0
+            else:
+                count += 1
         except KeyboardInterrupt:
             print "Exiting\n"
             sys.exit(0)
         except:
             pass
-        time.sleep(settings['delay'])
 
 if __name__ == "__main__":
     main()
---
+++ reporter.py
@@ -0,0 +1,122 @@
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
+
+
+class UserReport:
+
+    def __init__(self, user, suspicious_subs):
+        self.first = True
+        self.user = user
+        self.suspicious_subs = suspicious_subs
+        self.outString = None
+
+    def __str__(self):
+        return self.write()
+
+    def write(self):
+        if self.outString == None:
+            self.output = StringIO()
+            self._process_data()
+            self.outString = self.output.getvalue()
+            self.output.close()
+        return self.outString
+
+    def _process_data(self):
+        data = {}
+        post_dic = self.user.get_submitted().get_posts()
+        comment_dic = self.user.get_comments().get_posts()
+
+        for subreddit in sorted(post_dic, key=lambda k: len(post_dic[k]), reverse=True):
+            if subreddit.lower() in self.suspicious_subs:
+                if subreddit.lower() not in data:
+                    data[subreddit.lower()] = {}
+                data[subreddit.lower()]['posts'] = self.user.submitted.get_subreddit_posts(subreddit)
+
+
+        for subreddit in sorted(comment_dic, key=lambda k: len(comment_dic[k]), reverse=True):
+            if subreddit.lower() in self.suspicious_subs:
+                if subreddit.lower() not in data:
+                    data[subreddit.lower()] = {}
+                data[subreddit.lower()]['comments'] = self.user.comments.get_subreddit_posts(subreddit)
+
+        if len(data.keys()) < 1:
+            self.output.write("User has not posted in monitored subreddits")
+            return
+        else:
+            self.output.write("/u/%s " % self.user.username)
+            self.output.write("post history contains participation in the ")
+            self.output.write("following subreddits:\n\n")
+            for subreddit in sorted(data, key=lambda k: (len(data[k]['posts']), len(data[k]['comments'])), reverse=True):
+                self._write_data(subreddit, data[subreddit])
+            self.output.write(".")
+
+
+    def _write_data(self, subreddit, sub_data):
+        prefix = ""
+
+        if self.first:
+            self.first = False
+        else:
+            prefix = ".\n\n"
+
+        self.output.write("%s/r/%s: " % (prefix, subreddit))
+
+        if 'posts' in sub_data:
+            self._write_post_data(sub_data['posts'])
+        if 'comments' in sub_data:
+            if 'posts' in sub_data:
+                self.output.write("; ");
+            self._write_comment_data(subreddit, sub_data['comments'])
+
+    def _write_post_data(self, posts):
+
+        if len(posts) < 1:
+            return
+        elif len(posts) == 1:
+            score = posts[0].score
+            link = posts[0].permalink
+            link = link.replace("http://www.", "http://np.")
+            self.output.write("%d post ([1](%s)), **total score: %d;**" % (len(posts), link, score))
+        elif len(posts) > 1:
+            score = 0
+            self.output.write("%d posts (" % (len(posts)))
+
+            for post_counter in range(0, len(posts)):
+                prefix = "" if post_counter == 0 else  ", "
+                # limit due to a limit in reddit comment character length
+                if post_counter < 8:
+                    link = posts[post_counter].permalink
+                    link = link.replace("http://www.", "http://np.")
+                    self.output.write("%s[%d](%s)" % (prefix, post_counter + 1, link))
+                score += posts[post_counter].score
+
+            self.output.write("), **total score: %d**" % (score))
+
+
+    def _write_comment_data(self, subreddit, comments):
+
+        if len(comments) < 1:
+          return
+        elif len(comments) == 1:
+            score = comments[0].score
+            permalink = "http://www.reddit.com/r/%s/comments/%s/_/%s" % (subreddit, comments[0].link_id[3:], comments[0].id)
+            link = permalink.replace("http://www.", "http://np.")
+            self.output.write("%d comment ([1](%s))" % (len(comments), link))
+            self.output.write(", **total score: %d**" % score);
+        elif len(comments) > 1:
+            score = 0
+            self.output.write("%d comments (" % len(comments))
+
+            # loop through each of these comments
+            for comment_counter in range(0, len(comments)):
+                score += comments[comment_counter].score
+                permalink = "http://www.reddit.com/r/%s/comments/%s/_/%s" % (subreddit, comments[comment_counter].link_id[3:], comments[comment_counter].id)
+                link = permalink.replace("http://www.", "http://np.")
+                prefix = "" if comment_counter == 0 else ", "
+
+                if comment_counter < 8:
+                    self.output.write("%s[%d](%s)" % (prefix, comment_counter + 1, link))
+
+            self.output.write("), **combined score: %d**" % score)
